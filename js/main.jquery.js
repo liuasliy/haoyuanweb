cb4ee64644c97b61e91562f2f10b84fe05b91792
liuasliy <liuchao02@wondersgroup.com>
2021-07-25 15:39:13 +0800

    fix：fix

--- js/main.jquery.js
+++ js/main.jquery.js
@@ -59,4 +59,14 @@ $(function() {
         })
         $(this).hide()
     })
+
+    // $(".list_dt").on("click",function () {
+    //     $('.list_dd').stop();
+    //     $(this).siblings("dt").removeAttr("id");
+    //     if($(this).attr("id")=="open"){
+    //         $(this).removeAttr("id").siblings("dd").slideUp();
+    //     }else{
+    //         $(this).attr("id","open").next().slideDown().siblings("dd").slideUp();
+    //     }
+    // });
 })
